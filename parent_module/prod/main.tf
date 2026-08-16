0b0efb0f3744e2fe551d03f335c44121dac98864
shripalsinghdevops <shripal.singhdevops@gmail.com>
2026-08-16 20:22:31 +0530

    terraform CI CD update 2

--- parent_module/prod/main.tf
+++ parent_module/prod/main.tf
@@ -1,18 +1,18 @@
 module "resource_group" {
   source = "../../child_modules/azurerm_resource_group"
-rgs = var.rgs
+  rgs    = var.rgs
 
-  }
+}
 
 module "virtual_network" {
   source     = "../../child_modules/azurerm_virtual_network"
   depends_on = [module.resource_group]
-vnet = var.vnet
-    }
+  vnet       = var.vnet
+}
 module "subnetwork" {
   source     = "../../child_modules/azurerm_subnetwork"
   depends_on = [module.virtual_network]
-  subnet = var.subnet
+  subnet     = var.subnet
 
 }
 
